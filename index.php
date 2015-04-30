87afcc5320f7e38fc0d8334870e8cdada5c12cb0
Gabriel Matsuoka <gabriel.matsuoka@gmail.com>
2015-04-30 19:23:48 +0000

    add usuarios de teste

--- index.php
+++ index.php
@@ -10,10 +10,17 @@
         <ul id="example">
             <li><a href="frontend_pagar.php" target="iframe_example">Exemplo de Pagamento </a></li>
             <li><a href="frontend_oneclickpay.php" target="iframe_example">Exemplo de Pagamento (One Click Payment)</a></li>
-            <li><a href="standard_account_money.php" target="iframe_example">Exemplo de Checkout Standard com Account Money (Dinheiro em conta)</a> <p>Utilize a senha de retirada: 12345678</p></li>
+            <li>
+                <a href="standard_account_money.php" target="iframe_example">Exemplo de Checkout Standard com Account Money (Dinheiro em conta)</a>
+                <p>Efetue o login no MercadoPago com o usuario:</p>
+                <p>Usuario: test_user_40076148@testuser.com</p>
+                <p>Senha: qatest8150</p>
+                <p>Utilize a senha de retirada: 12345678</p>
+            </li>
             <li><a href="get.php" target="iframe_example">Get Pagamento</a></li>
             <li><a href="search.php" target="iframe_example">Search de Pagamento</a></li>
             <li><a href="refunds.php" target="iframe_example">Devolução de Pagamento</a></li>
+            <li><a href="usuarios_teste.json" target="iframe_example">Usuarios de teste</a></li>
         </ul>
         
         <iframe name="iframe_example" id="iframe_example">
